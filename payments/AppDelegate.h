fd3edc2ab743f4e2ca73c23e38e9402aabff6936
Chan Koh <koh.chanhee@gmail.com>
2017-05-08 21:08:52 -0700

    Initial Commit

---
+++ payments/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  payments
+//
+//  Created by Chan on 5/8/17.
+//  Copyright © 2017 Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ payments/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  payments
+//
+//  Created by Chan on 5/8/17.
+//  Copyright © 2017 Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
